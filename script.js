5f5253ecbb0e9deb225ff18decaf142f78de907b
Rish-2001 <121566349+Rish-2001@users.noreply.github.com>
2023-03-26 03:23:44 +0530

    blogs

--- script.js
+++ script.js
@@ -20,6 +20,7 @@ var swiper = new Swiper(".home-slider", {
       el: ".swiper-pagination",
       clickable: true,
     },
+    
   });
 
   var swiper = new Swiper(".review-slider", {
@@ -27,7 +28,7 @@ var swiper = new Swiper(".home-slider", {
     grabCursor: true,
     loop:true,
     autoplay: {
-        delay: 7500,
+        delay: 1000,
         disableOnInteraction: false,
     },
     breakpoints:{
@@ -36,6 +37,32 @@ var swiper = new Swiper(".home-slider", {
         },
         600:{
             slidesPerView:2,
+        },
+    },
+});
+
+
+var swiper = new Swiper(".blogs-slider", {
+    spaceBetween: 20,
+    grabCursor: true,
+    loop:true,
+    autoplay: {
+        delay: 1000,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    breakpoints:{
+        0:{
+            slidesPerView:1,
+        },
+        768:{
+            slidesPerView:2,
+        },
+        991:{
+            slidesPerView:3,
         },
     },
 });
